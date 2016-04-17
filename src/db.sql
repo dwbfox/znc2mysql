09a92942cb32f14f85953565e013caea409551d3
dwbfox <dwb.misc@gmail.com>
2016-04-17 13:34:42 -0500

    Merged uncessary files and did some code cleanup

---
+++ src/db.sql
@@ -0,0 +1,53 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `messages`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `channel` varchar(30) NOT NULL,
+  `user` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=269058 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nick` varchar(25) NOT NULL,
+  `last_seen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_message` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nick` (`nick`)
+) ENGINE=InnoDB AUTO_INCREMENT=269052 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ src/znc2mysql.py
@@ -0,0 +1,133 @@
+import znc
+import re
+import sys
+import os
+import pretty
+import logging
+import pymysql.cursors
+import json
+import datetime
+
+###########################################
+#  MySQL class to handle 
+#  Database connection and recording
+###########################################
+class zncmysql:
+
+    connection = None
+    logger = None
+
+    def __init__(self):
+        logger = logging.basicConfig(filename='irc2mysql.log', level=logging.DEBUG)
+        try:
+            self.connection = pymysql.connect(
+                host='localhost',
+                user='root',
+                password='root',
+                db='irc',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        except Exception as e:
+            logger.warning('Unable to connect to database: %s' % str())
+
+
+    def insertMessage(self, user, channel, message):
+        uid = None
+        # Get the ID of the user
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute("SELECT id FROM `users` WHERE nick=%s LIMIT 1", user)
+                result = cursor.fetchone()
+                uid = result['id']
+            except:
+                pass # TODO: handle this case
+
+        self.connection.commit()
+
+
+    def userLastSeen(self, nick):
+        with self.connection.cursor() as cursor:
+            try:
+                seen = {}
+                uid = self.getUIDByNick(nick)
+                cursor.execute("SELECT last_seen, last_message FROM `users` WHERE nick=%s", nick)
+                results = cursor.fetchone()
+                seen['seen'] = pretty.date(results['last_seen'], short=False)
+                seen['message'] = results['last_message']
+                return seen
+            except Exception as e:
+                print('Error: %s', str(e))
+                return None
+
+
+    def getUIDByNick(self, nick):
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute("SELECT id FROM `users` WHERE nick=%s LIMIT 1", nick)
+                result = cursor.fetchone()
+                return result['id']
+            except:
+                return None
+
+
+    def insertMessage(self, user, channel, message):
+        uid = self.getUIDByNick(user)
+        self.connection.commit()
+        with self.connection.cursor() as cursor:
+            cursor.execute("INSERT INTO `messages` (`channel`, `user`, `message`) VALUES (%s, %s, %s)", (channel, uid, message))
+        self.connection.commit()
+
+
+    def insertUser(self, user, message):
+        with self.connection.cursor() as cursor:
+            cursor.execute("INSERT INTO `users` (`nick`, `last_message`) VALUES (%s, %s) ON DUPLICATE KEY UPDATE last_message=%s", (user, message, message))
+        self.connection.commit()
+
+
+    def getTimeStamp(self):
+        return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+    def __del__(self):
+        self.connection.close()
+
+
+###########################################
+#   Main znc module subclass
+###########################################
+class znc2mysql(znc.Module):
+    description = "Store IRC logs to a MySQL database"
+    regex = re.compile(b'^\seen (.+)$')
+    logger = None
+
+    def OnLoad(self, args, msg):
+        self.PutModule('<<irc2mysql loaded.>>')
+        self.PutModule('<<Dir: %s' % os.getcwd())
+        return znc.CONTINUE
+
+
+    def OnChanMsg(self, nick, channel, message):
+        try:
+            msg = message.s.encode('utf-8')
+            name = nick.GetNick()
+            chan = channel.GetName()
+            ircdb = zncmysql()
+            ircdb.insertUser(name, msg)
+            ircdb.insertMessage(name, chan, msg)
+            matches = self.regex.match(msg)
+            if matches is not None:
+                queried_nick = matches.group(1)
+                seen = db.userLastSeen(queried_nick)
+                if seen is None:
+                    logger.info('Queried user not found: %s' % queried_nick)
+                    self.PutModule('Queried user not found: %s' % queried_nick)
+                else:
+                    self.PutModule('%s was last seen %s saying, "%s"' % (queried_nick, seen['seen'], seen['message']))
+                    logger.info('%s was last seen %s saying, "%s"' % (queried_nick, seen['seen'], seen['message']))
+        except Exception as e:
+            logger.info('<<error>> %s' % str(e))
+        finally:
+            return znc.CONTINUE
+
+
+
